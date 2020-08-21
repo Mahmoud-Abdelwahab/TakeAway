4b656e5b64224133efcb3680c18c16fdcca985db
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2020-08-21 22:39:33 +0200

    initial commit
    Co-Authored-By: Mahmoud-Abdelwahab <17813115+mahmoud-abdelwahab@users.noreply.github.com>

---
+++ Shared/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  TakeAway
+//
+//  Created by kasper on 8/21/20.
+//  Copyright © 2020 Mahmoud Abdul-Wahab. All rights reserved.
+//
+
+import Foundation
---
+++ TakeAwayAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TakeAwayAdmin
+//
+//  Created by kasper on 8/21/20.
+//  Copyright © 2020 Mahmoud Abdul-Wahab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
