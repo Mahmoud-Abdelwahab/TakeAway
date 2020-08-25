adca471f34d0dc8529f5be97d22a926b3d5fce9a
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2020-08-25 08:42:39 +0200

    setup UI and Logic
    Co-Authored-By: Mahmoud-Abdelwahab <17813115+mahmoud-abdelwahab@users.noreply.github.com>

---
+++ TakeAway/ProductDetails/ProductDetailsVC.swift
@@ -0,0 +1,47 @@
+//
+//  ProductDetailsVC.swift
+//  TakeAway
+//
+//  Created by kasper on 8/25/20.
+//  Copyright © 2020 Mahmoud Abdul-Wahab. All rights reserved.
+//
+
+import UIKit
+import  Kingfisher
+
+class ProductDetailsVC: UIViewController {
+    
+    var product:Products?{
+        didSet{
+            configureForm(with: product!)
+        }
+    }
+    
+    @IBOutlet weak var productName: UILabel!
+    
+    @IBOutlet weak var productDescription: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productStock: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func onAddProductToCartClicked(_ sender: Any) {
+        
+    }
+    
+    private func configureForm(with product : Products) {
+        
+        productName.text        = product.name
+        productDescription.text = product.productDescription
+        productStock.text       = "\(product.stock ?? 0)"
+        productPrice.text       = "\(product.price)"
+        guard let url           = URL(string: product.imgURL!) else{return}
+        
+        productImage.kf.setImage(with:url)
+        
+    }
+    
+}
